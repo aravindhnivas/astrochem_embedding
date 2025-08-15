a7208ce3afb39eb0331e6164ab2ba6fa2487167c
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2025-08-15 15:24:34 -0400

    minor bug fix for intel macos

--- src/astrochem_embedding/__init__.py
+++ src/astrochem_embedding/__init__.py
@@ -6,7 +6,7 @@ from astrochem_embedding import models, pipeline, layers
 from astrochem_embedding.models.models import VICGAE, GRUAutoEncoder
 from astrochem_embedding.pipeline.data import MaskedStringDataModule, StringDataModule
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = [
     "get_paths",
     "Translator",
